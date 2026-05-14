2fc4d0c0591dd5effa1fd9c9c99c4ad800bd03ff
Parent <julien.parent@studentfr.ch>
2026-05-14 18:46:06 +0200

    commit avant de partir

--- src/app/models/Commandes.java
+++ src/app/models/Commandes.java
@@ -1,3 +1,12 @@
 public class Commandes {
+    private final int NBRE_MAX_CAGEOTS = 8;
+
+    private Cageots[] commande2Cageots;
+    private final Clients client;
+
+    public Commandes(Cageots[] commande2Cageots, Clients client) {
+        this.commande2Cageots = new Cageots[NBRE_MAX_CAGEOTS];
+        this.client = client;
+    }
     
 }
